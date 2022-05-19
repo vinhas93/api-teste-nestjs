1902ed17792b4c1a982be8909795e64e5d42d718
Daniel Vinhas <vinhas.daniel@gmail.com>
2022-05-18 22:30:49 -0300

    feat(hero): created entity

---
+++ src/Hero/entities/hero.entity.ts
@@ -0,0 +1,8 @@
+export class Hero {
+  id: number;
+  name: string;
+  age: number;
+  class: string;
+  skill: string;
+  story: string;
+}
--- src/Hero/hero.service.ts
+++ src/Hero/hero.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateHeroDto } from './dto/create-hero.dto';
+import { Hero } from './entities/hero.entity';
 
 @Injectable()
 export class HeroService {
+  heroes: Hero[] = [];
   findAll() {
     return 'Buscar todos os Heróis.';
   }
 
   create(createHeroDto: CreateHeroDto) {
-    return 'Herói criado: ' + JSON.stringify(createHeroDto);
+    const hero: Hero = { id: this.heroes.length + 1, ...createHeroDto };
+
+    this.heroes.push(hero);
+
+    return 'Um novo herói chega ao mundo: /n' + JSON.stringify(hero);
   }
 }
